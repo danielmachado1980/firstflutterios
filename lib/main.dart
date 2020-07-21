c4832460bf635774657afbdc370781a7f2a5cd8e
Daniel <daniel@Daniels-iMac.local>
2020-07-20 23:57:31 -0300

    Alteracao de label

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You touched the button this many times:',
+              'You has touched the button this many times:',
             ),
             Text(
               '$_counter',
