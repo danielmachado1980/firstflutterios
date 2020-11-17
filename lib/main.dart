087684a91369624cf3a2609f84a93b4b150e1a8d
Daniel <daniel@Daniels-iMac.local>
2020-11-16 21:59:30 -0300

    Alteracao de label

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter - Android & iOS'),
+      home: MyHomePage(title: 'Flutter Android & iOS'),
     );
   }
 }
